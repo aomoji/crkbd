772000f786d2c0026a7f1c32d2eac5c6fd9a4b49
aomoji <41809401+aomoji@users.noreply.github.com>
2021-07-12 13:13:48 +0900

    enabled to switch virtual desktop on mac

--- keymap.c
+++ keymap.c
@@ -47,19 +47,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC, XXXXXXX, XXXXXXX, KC_TILD, KC_PIPE, KC_LCBR,                      KC_RCBR, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, XXXXXXX,\
+       KC_ESC, XXXXXXX, XXXXXXX, KC_TILD, KC_PIPE, KC_LCBR,                      KC_RCBR, KC_UNDS, KC_PLUS, XXXXXXX, KC_MPLY, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, XXXXXXX, KC_MPLY,    KC_MPLY,  XXXXXXX, KC_RGUI \
+                                          KC_LALT, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, KC_RGUI \
                                       //`--------------------------'  `--------------------------'
     ),
 
   [2] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    LCTL(LGUI(KC_Q)), XXXXXXX, XXXXXXX, KC_GRV, KC_BSLS, KC_LBRC,                KC_RBRC, KC_MINS,  KC_EQL, XXXXXXX, XXXXXXX, KC_BSPC,\
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_GRV, KC_BSLS, KC_LBRC,                       KC_RBRC, KC_MINS,  KC_EQL, XXXXXXX, XXXXXXX, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -71,13 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,KC_LANG2,                     KC_LANG1, LALT(KC_LEFT), LALT(KC_RIGHT), RGUI(KC_SPACE), KC_F14, KC_F15,\
+    LCTL(LGUI(KC_Q)),LCTL(KC_1), LCTL(KC_2),LCTL(KC_3),LCTL(KC_4),LCTL(KC_5),       XXXXXXX, LALT(KC_LEFT), LALT(KC_RIGHT), RGUI(KC_SPACE), KC_F14, KC_F15,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,KC__VOLDOWN, KC__VOLUP, KC_AUDIO_MUTE, LGUI(KC_RBRC), XXXXXXX,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, LGUI(LSFT(KC_C)), XXXXXXX, LGUI(KC_LBRC),       XXXXXXX, LCTL(RSFT(KC_TAB)), LCTL(KC_TAB), XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_RGUI \
+                                          KC_LALT, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_RGUI \
                                       //`--------------------------'  `--------------------------'
   )
 };
