82a6dd8f4e6069b1f3728c479258b34c1f86b49b
Kaito Oshiro <k.oshiro@churadata.okinawa>
2024-11-05 16:02:48 +0900

    added alt key to right side

--- keymap.c
+++ keymap.c
@@ -34,13 +34,13 @@ enum macro_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-  LALT_T(KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
+      KC_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SFT_T(KC_QUOT),\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, LOWER,  LT(3, KC_SPC),  LT(3, KC_SPC),  RAISE,  KC_RGUI \
+                                          KC_LGUI, LOWER,  LT(3, KC_SPC),  LT(3, KC_SPC),  RAISE,  KC_RALT \
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, KC_RGUI \
+                                          KC_LGUI, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, KC_RALT \
                                       //`--------------------------'  `--------------------------'
     ),
 
@@ -65,19 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, LGUI(LSFT(LCTL(KC_4))), LGUI(LSFT(KC_4)), XXXXXXX, KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_RGUI \
+                                          KC_LGUI, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_RALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
   [3] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    LCTL(LGUI(KC_Q)),LCTL(KC_1), LCTL(KC_2),LCTL(KC_3),LCTL(KC_4),LCTL(KC_5),       XXXXXXX, LALT(KC_LEFT), LALT(KC_RIGHT), RGUI(KC_SPACE), KC_F14, KC_F15,\
+      XXXXXXX,LCTL(KC_1), LCTL(KC_2),LCTL(KC_3),LCTL(KC_4),LCTL(KC_5),       XXXXXXX, LALT(KC_LEFT), LALT(KC_RIGHT), RGUI(KC_SPACE), KC_F14, KC_F15,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,KC_VOLD, KC_VOLU, KC_AUDIO_MUTE, LGUI(KC_RBRC), XXXXXXX,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, LGUI(LSFT(KC_C)), XXXXXXX, LGUI(KC_LBRC),       XXXXXXX, LCTL(RSFT(KC_TAB)), LCTL(KC_TAB), XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_RGUI \
+                                          KC_LGUI, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_RALT \
                                       //`--------------------------'  `--------------------------'
   )
 };
